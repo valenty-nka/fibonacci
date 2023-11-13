439fd9737916850b061255404db12e80be17e880
valenty-nka <valenty.nechyporenko@gmail.com>
2023-11-13 16:14:35 -0400

    Creat fibonacci.js file and the code to it. Modify README.md file to add description of the project.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fibonacci
+
 how to solve fibonacci (js)
+
+You can use memoization which basically means you create an empty object and store repeated actions so it take much less time then O(2 power n).
---
+++ fibonacci.js
@@ -0,0 +1,11 @@
+//using memoization
+const fib = (n, memo = {}) => {
+  if (n in memo) {
+    return memo[n];
+  }
+  if (n < 2) {
+    return n;
+  }
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
+};
